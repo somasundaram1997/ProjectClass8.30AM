d95db8776b657c78de78f04a02e2a12f6afef37d
somasundaram1997 <somasundarams23@gmail.com>
2022-06-08 19:41:10 +0530

    new code

--- src/test/java/org/test/maven/junit/day1/FacebookAssert.java
+++ src/test/java/org/test/maven/junit/day1/FacebookAssert.java
@@ -1,5 +1,31 @@
 package org.test.maven.junit.day1;
 
-public class FacebookAssert {
+import java.util.Date;
 
+import org.junit.*;
+
+import org.test.base.BaseClass;
+
+
+public class FacebookAssert extends BaseClass{
+	@BeforeClass
+	public static void beforeClass() {
+		browserLaunch("chrome");
+	}
+	@AfterClass
+	public static void afterClass() {
+		driver.quit();
+	}
+	@Before
+	public void before() {
+		System.out.println("Test starts at......."+new Date());
+	}
+	@After
+	public void after() {
+		System.out.println("Test ends at......."+new Date());
+
+	}
+	
+	
+	
 }
